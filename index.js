2cb0c3a581b6e84387df2b43a8ededc4288bc071
Rishabh Verma <114663677+mintpapa@users.noreply.github.com>
2025-04-29 19:32:59 +0530

    fixed the goddamn backend bug and proceeded further

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const Product=require('./models/product.models.js')
 
 app.use(express.json())
 
+
+
+
+app.use("/api/products",productRoute)
+
 app.listen(3000,()=>{
     console.log("Server is running on port 3000")
 }
@@ -18,9 +23,68 @@ app.get("/",(req,res)=>{
 app.post('/api/products',async(req,res)=>{
     try {
         const product=await Product.create(req.body)
+        res.status(201).json(product)
     } catch (error) {
         res.status(500).json({message:error.message})
         
+    }
+})
+
+app.get('/api/products',async(req,res)=>{
+    try{
+        const products =await Product.find({})
+        res.status(200).json(products)
+
+    }
+    catch(error){
+        res.status(500).json({message:error.message})
+    }
+})
+
+app.get('/api/products/:id',async(req,res)=>{
+    try{
+        const {id}=req.params
+        const product=await Product.findById(id)
+        res.status(200).json(product)
+
+    }
+    catch(error){
+        res.status(500).json({message:error.message})
+    }
+})
+
+//update product
+
+app.put('/api/products/:id',async(req,res)=>{
+    try{
+        const {id}=req.params
+        const product=await Product.findByIdAndUpdate(id,req.body)
+
+        if(!product){
+            return res.status(404).json({message:'not found'})
+        }
+        const updatedProduct=await Product.findById(id)
+        res.status(200).json(updatedProduct)
+    }
+    catch(error){
+        res.status(500).json({message:error.message})
+    }
+})
+
+//delete product
+
+app.delete('/api/products/:id',async(req,res)=>{
+    try{
+        const {id}=req.params
+        const product=await Product.findByIdAndDelete(id)
+
+        if(!product){
+            return res.status(404).json({mesage:"not found"})
+        }
+        res.status(200).json({message:"product deleted succesfully"})
+    }
+    catch(error){
+        res.status(500).json({message:error.message})
     }
 })
 
